bff58f2ddf8575b50801eed8271fb1a662caaec8
Andrey <dovban.andreyy@rambler.ru>
2023-06-17 17:51:45 +0300

    fix

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const { resolve } = require('path');
 const server = express();
 server.use(express.static(resolve(__dirname, './dist')));
 
-server.get('/', (req, res) =>
-    res.sendFile(resolve(__dirname, './dist', 'index.html')),
-);
+server.get('/', (req, res) => {
+    res.sendFile(resolve(__dirname, './dist', 'index.html'));
+});
 
-server.use((req, res) => res.send('error'));
+server.use((req, res) => {
+    res.send('error');
+});
 
 server.listen(3000);
